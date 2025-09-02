d1749380b9600e61508d3bc793ed103898f7a912
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-09-02 11:53:15 +0530

    Updates from Morning Session of Day 6.

---
+++ src/CSharpConcepts/Demo_DynamicType/Program.cs
@@ -0,0 +1,60 @@
+﻿#nullable disable
+
+namespace Demo_DynamicType
+{
+    internal class Program
+    {
+        static void Main ( string[] args )
+        {
+            Demo1();
+            Console.WriteLine();
+
+            Demo2();
+            Console.WriteLine();
+
+            Demo3();
+            Console.WriteLine();
+        }
+
+
+        /// <summary>
+        ///     Using Explicit Type
+        /// </summary>
+        static void Demo1 ()
+        {
+            Calculator objCalc = new Calculator();
+            int result = objCalc.Add( 10, 20 );
+            Console.WriteLine( "Sum of 10 and 20 is {0}", result );
+        }
+
+        /// <summary>
+        ///     Using Reflection
+        /// </summary>
+        static void Demo2 ()
+        {
+            object calc = System.Activator.CreateInstance( typeof( Calculator ) ); // = new Calculator();
+            Type calcType = calc.GetType();
+            object res = calcType.InvokeMember( "Add",
+                System.Reflection.BindingFlags.InvokeMethod, null, calc,
+                new object[] { 10, 20 } );
+            int result = Convert.ToInt32( res );
+            Console.WriteLine( "Sum of 10 and 20 is {0}", result );
+        }
+
+        /// <summary>
+        ///     Using dynamic type
+        /// </summary>
+        static void Demo3 ()
+        {
+            dynamic d1 = System.Activator.CreateInstance( typeof( Calculator ) );
+            int result = d1.Add( 10, 20 );
+            Console.WriteLine( "Sum of 10 and 20 is {0}", result );
+
+            object d2 = System.Activator.CreateInstance( typeof( Calculator ) );
+
+            dynamic calc = new Calculator();
+            int result2 = calc.Add( 10, 20 );
+            Console.WriteLine( "Sum of 10 and 20 is {0}", result2 );
+        }
+    }
+}
---
+++ src/CSharpConcepts/Demo_NewTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace Demo_NewTypes
+{
+    /// <summary>
+    ///     Demo of New Types introduced in C# 4.0
+    /// </summary>
+    internal class Program
+    {
+        static void Main ( string[] args )
+        {
+            var oTuple = new Tuple<string, int>( "Manoj Sharma", 39 );
+            Console.WriteLine( "Item 1 is {0} of Type: {1}",
+                oTuple.Item1, oTuple.Item1.GetType().ToString() );
+            Console.WriteLine( "Item 2 is {0} of Type: {1}",
+                oTuple.Item2, oTuple.Item2.GetType().ToString() );
+            //  oTuple.Item1 = oTuple.Item1.ToUpper();                  // READONLY PROPERTIES
+
+
+            Console.WriteLine();
+
+            System.Numerics.Complex num = new Complex( 1, 2 );
+            Console.WriteLine( num );
+            Console.WriteLine( "Imaginary: {0}", num.Imaginary );
+            Console.WriteLine( "Magnitude: {0}", num.Magnitude );
+            Console.WriteLine( "Phase : {0}", num.Phase );
+            Console.WriteLine( "Real : {0}", num.Real );
+            Console.WriteLine();
+
+            Console.Write( "Press any key to continue..." );
+            Console.ReadKey();
+
+            Console.WriteLine( GetFactorial( 6000 ) );
+            Console.WriteLine();
+        }
+
+        static System.Numerics.BigInteger GetFactorial ( int num )
+        {
+            return num == 0 ? new BigInteger( 1 ) : num * GetFactorial( num - 1 );
+        }
+    }
+}
