02827ded3b8eea341642356ca0c50c7b86797b70
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-09-03 05:56:11 +0530

    Day 7 - Part 3
    - Register the HttpClientModule for Dependency Injection

--- src/AngularConcepts/ClientWebApplication1/AngularTSProject/src/app/app-module.ts
+++ src/AngularConcepts/ClientWebApplication1/AngularTSProject/src/app/app-module.ts
@@ -1,6 +1,9 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import the HttpClientModule class
+import { provideHttpClient } from '@angular/common/http';         
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
@@ -13,7 +16,8 @@ import { App } from './app';
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient()                          // ensures that HttpClient is provided in the Services through DI
   ],
   bootstrap: [App]
 })
