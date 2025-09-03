27e294e17f289002fd8d66124dc717a5c76260ab
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-09-03 06:04:00 +0530

    Day 7 - Part 4
    - Added the Hello Service which receives the HttpClientModule

---
+++ src/AngularConcepts/ClientWebApplication1/AngularTSProject/src/app/hello.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelloService {
+
+  private apiUrl = 'https://localhost:7160/';
+
+  // Constructor receives the HttpClientModule from the DI provider container.
+  constructor(private http: HttpClient) {
+  }
+
+
+  getHello(): Observable<string> {
+
+    console.log("getHello() called!!!")
+
+    return this.http.get(this.apiUrl, { responseType: 'text' });
+  }
+
+}
