98a9812b2d4600530b379fea173a1d0de3c29fe7
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-09-03 06:10:57 +0530

    Day 7 - Part 5
    - Updated the App Component File.
    NOTE: on running we get CORS error.

--- src/AngularConcepts/ClientWebApplication1/AngularTSProject/src/app/app.ts
+++ src/AngularConcepts/ClientWebApplication1/AngularTSProject/src/app/app.ts
@@ -1,11 +1,41 @@
-import { Component, signal } from '@angular/core';
+// This is the App.Component file.
+
+import { Component, OnInit, signal } from '@angular/core';
+import { HelloService } from './hello.service';                 // register the HelloService
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.html',
+  // templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+
+  template:
+    `
+      <h1>{{ title }}</h1>
+      <h3>{{ message }}</h3>
+      <button (click)="loadMessage()">
+        Get message from API
+      </button>
+    `
+
 })
-export class App {
-  protected readonly title = signal('AngularTSProject');
+
+export class App implements OnInit {
+
+  protected readonly title = 'My Angular SPA for WebApplication1';
+  message = 'Loading';
+
+  constructor(private helloService: HelloService) { }
+
+  ngOnInit() {
+    this.loadMessage();
+  }
+
+  loadMessage() {
+    this.helloService.getHello().subscribe({
+      next: (data) => this.message = data,
+      error: (err) => this.message = 'Error: ' + err.message
+    });
+  }
+
 }
